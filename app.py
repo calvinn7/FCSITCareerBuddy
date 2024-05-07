aa1e3954de74d098d7e1759e80ef8e380020ae71
Calvin993 <calvinjmy993@gmail.com>
2024-05-07 19:49:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ import streamlit as st
 from job_recommendation import recommend_jobs
 from data_preprocessing import load_job_data
 from data_preprocessing import load_events
+
+st.set_page_config(page_title="FCSIT Career Buddy", page_icon=":technologist:", layout="wide")
+
 def main():
-    st.title("FCSIT Career Buddy")
-    st.header("Your Personal AI Compass to Navigate Your Career")
+    # -- header --- 
+    st.title ("Welcome to the FCSIT Career Bot :wave:")
+    st.subheader ("Your personal AI-powered career companion ")
+    st.write ("Check out our GitHub repo [here](https://github.com/calvinn7/FCSITCareerBuddy)")
     
+    # Clear conversation history at the beginning of each session
+    if 'conversation' not in st.session_state:
+        st.session_state.conversation = []
+    else:
+        st.session_state.conversation.clear()
+
     # Load job data
     job_data = load_job_data('dataset/job_data.csv')
     events= load_events ('dataset/networking_events.csv')
-
-    # Initialize conversation state
-    if 'conversation' not in st.session_state:
-        st.session_state.conversation = []
     
     # Display previous conversation
     for i in range(len(st.session_state.conversation)):
@@ -27,22 +34,21 @@ def main():
     
      # Check user input for different commands
     if user_input.lower() == "hello":
-            response = "Hello! How can I assist you today?"
+        response = "Hello! How can I assist you today?"
     elif "networking event" in user_input.lower() or "networking events" in user_input.lower():
-            response = "Here are some upcoming networking events:"
-            # Display recommended jobs in a table
-            st.header("Upcoming Networking Events:")
-            for _, event in events.iterrows():
-                st.write(f"**Event:** {event['event']}")
-                st.write(f"**Date:** {event['date']}")
-                st.write(f"**Location:** {event['location']}")
-                st.write(f"**Details:** {event['details']}")
-                st.write("---") 
-        
+        response = "Here are some upcoming networking events:"
+        # Display networking events in a table
+        st.header("Upcoming Networking Events:")
+        for _, event in events.iterrows():
+            st.write(f"**Event:** {event['event']}")
+            st.write(f"**Date:** {event['date']}")
+            st.write(f"**Location:** {event['location']}")
+            st.write(f"**Details:** {event['details']}")
+            st.write("---") 
     elif user_input.lower() == "faq":
-            response = "Here are some frequently asked questions:"
-            # Logic to fetch and display FAQs
-    else:
+        response = "Here are some frequently asked questions:"
+        # Logic to fetch and display FAQs
+    elif user_input.strip():  # Check if user input is not empty or only whitespace
         # Recommend jobs based on user input
         recommended_jobs = recommend_jobs(user_input, job_data)
         
@@ -57,10 +63,12 @@ def main():
             st.write(f"**Job URL:** [{job['job_url']}]({job['job_url']})")
             st.write(f"**Company rating:** {job['overall_rating']} from {job['num_ratings']} reviews")
             st.write("---")
+    else:
+        response = "I'm sorry, I didn't understand that. Can you please rephrase or ask something else?"
         
     # Update conversation state
     st.session_state.conversation.append(user_input)
-    st.session_state.conversation.append("Here are some recommended jobs based on your input.")
+    st.session_state.conversation.append(response)
 
 if __name__ == "__main__":
     main()
