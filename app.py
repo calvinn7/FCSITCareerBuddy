ee27f4a68d49d556956ce1054c0d09d6c2a1cb41
Calvin993 <calvinjmy993@gmail.com>
2024-06-02 19:06:39 +0800

    integrated welcome page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import base64
-import os.path
+#import libraries
 import asyncio
 import traceback
-import random
 import streamlit as st
+
+#import functions from modules
 from models.job_recommendation import recommend_jobs
 from models.faq_model import get_most_similar_question
 from data_preprocessing import load_job_data
@@ -12,81 +12,12 @@ from data_preprocessing import preprocess_text
 from intent_classification import classify_intent
 from responses import get_greet_response, get_bye_response
 from user_feedback import feedback
+from welcome_screen import render_welcome_screen
+from utils import get_star_rating,get_css,clear_chat,get_chat_message,type_reply
 
-# Set global variables
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DEBUG = False
 
 st.set_page_config(page_title="FCSIT Career Buddy", page_icon=":technologist:", layout="wide")
-
-@st.cache_data(show_spinner=False)
-def get_local_img(file_path: str) -> str:
-    # Load a byte image and return its base64 encoded string
-    return base64.b64encode(open(file_path, "rb").read()).decode("utf-8")
-
-def get_star_rating(rating):
-    filled_stars = '⭐' * round(rating)
-    empty_stars = '⭒' * (5 - round(rating))
-    return f"{filled_stars}{empty_stars}"
-
-@st.cache_data(show_spinner=False)
-def get_css() -> str:
-
-    # Read CSS code from style.css file
-    with open(os.path.join(ROOT_DIR, "FCSITCareerBuddy" ,  "style.css"), "r") as f:
-        return f"<style>{f.read()}</style>"
-
-def clear_chat():
-    st.session_state.LOG = ["Hello! How can I assist you today?"]
-    st.session_state.MEMORY = [{'role': "system", 'content': "Hello! How can I assist you today?"}]
-    st.session_state.SHOW_FEEDBACK_FORM = False
-    st.session_state.stop = False
-
-def get_chat_message(
-        contents: str = "",
-        align: str = "left"
-) -> str:
-
-    # Formats the message in an chat fashion (user right, reply left)
-    div_class = "AI-line"
-    color = "rgb(54, 65, 85)"
-    file_path = os.path.join(ROOT_DIR, "FCSITCareerBuddy" , "assets", "bot.png")
-    src = f"data:image/gif;base64,{get_local_img(file_path)}"
-
-    if align == "right":
-        div_class = "human-line"
-        color = "rgb(51, 78, 255)"
-        if "USER" in st.session_state:
-            src = st.session_state.USER.avatar_url
-        else:
-            file_path = os.path.join(ROOT_DIR, "FCSITCareerBuddy" ,"assets", "user_icon.png")
-            src = f"data:image/gif;base64,{get_local_img(file_path)}"
-    icon_code = f"<img class='chat-icon' src='{src}' width=32 height=32 alt='avatar'>"
-    formatted_contents = f"""
-        <div class="{div_class}">
-            {icon_code}
-            <div class="chat-bubble" style="background: {color}; color: white;">
-            &#8203;{contents}
-        """
-    return formatted_contents
-
-async def type_reply(reply_box, message):
-    typing_indicator = "<div class='typing-indicator'>Career Buddy is typing...</div>"
-    reply_box.markdown(get_chat_message(typing_indicator), unsafe_allow_html=True)
-    typed_message = ""
-    for char in message:
-        typed_message += char
-        reply_box.markdown(get_chat_message(typed_message), unsafe_allow_html=True)
-        await asyncio.sleep(0.0024)
-    reply_box.markdown(get_chat_message(typed_message), unsafe_allow_html=True)
-
-with st.sidebar:
-    if st.button("Clear Chat"):
-        clear_chat()
-        INITIAL_PROMPT = "Hello! How can I assist you today?"
-        st.session_state.LOG = [INITIAL_PROMPT]
-        st.session_state.LOG.append(f"AI: {INITIAL_PROMPT}")
-        st.session_state.MEMORY = [{'role': "system", 'content': INITIAL_PROMPT}]
+DEBUG = False
 
 #when user inputs
 async def main(human_prompt: str) -> dict:
@@ -196,43 +127,6 @@ async def main(human_prompt: str) -> dict:
 
 ### MAIN STREAMLIT UI STARTS HERE ###
 
-# Define main layout
-gradient_text_html = """
-<style>
-.gradient-text {
-    font-weight: bold;
-    background: -webkit-linear-gradient(left, rgb(51, 78, 255), rgb(197, 153, 233));
-    background: linear-gradient(to right, rgb(51, 78, 255), rgb(197, 153, 233));
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    display: inline;
-    font-size: 3em;
-}
-</style>
-<div class="gradient-text">Welcome to the FCSIT Career Buddy</div>
-"""
-st.markdown(gradient_text_html, unsafe_allow_html=True)
-st.subheader ("Your personal AI-powered career companion ")
-chat_box = st.container()
-st.write("")
-prompt_box = st.empty()
-
-#sidebar
-with open("sidebar.md", "r") as sidebar_file:
-    sidebar_content = sidebar_file.read()
-
-with open("style.css", "r") as styles_file:
-    styles_content = styles_file.read()
-
-st.sidebar.markdown(sidebar_content)
-st.write(styles_content, unsafe_allow_html=True)
-
-# User feedback in the sidebar
-st.sidebar.header("Feedback")
-st.sidebar.markdown("We value your feedback. Click the feedback button to start your feedback!")
-if(st.sidebar.button("User Feedback")):
-    st.session_state.SHOW_FEEDBACK_FORM = True
-
 # Load CSS code
 st.markdown(get_css(), unsafe_allow_html=True)
 
@@ -245,6 +139,8 @@ if "MEMORY" not in st.session_state:
     st.session_state.MEMORY = [{'role': "system", 'content': INITIAL_PROMPT}]
     st.session_state.SHOW_FEEDBACK_FORM = False
     st.session_state.stop = False
+    st.session_state.show_welcome = True
+
 
 # JavaScript code to focus on the chat input textarea
 js_code = """
@@ -255,38 +151,86 @@ js_code = """
     }
 </script>
 """
+if st.session_state.show_welcome:
+    render_welcome_screen()
+else:
+    # Define main layout
+    gradient_text_html = """
+    <style>
+    .gradient-text {
+        font-weight: bold;
+        background: -webkit-linear-gradient(left, rgb(51, 78, 255), rgb(197, 153, 233));
+        background: linear-gradient(to right, rgb(51, 78, 255), rgb(197, 153, 233));
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        display: inline;
+        font-size: 3em;
+    }
+    </style>
+    <div class="gradient-text">Welcome to FCSIT Career Buddy</div>
+    """
+    st.markdown(gradient_text_html, unsafe_allow_html=True)
+    st.subheader ("Your personal AI-powered career companion ")
+    chat_box = st.container()
+    st.write("")
+    prompt_box = st.empty()
 
-# Render chat history so far
-with chat_box:
-    for line in st.session_state.LOG[1:]:
-        # For AI response
-        if line.startswith("AI: "):
-            contents = line.split("AI: ")[1]
-            st.markdown(get_chat_message(contents), unsafe_allow_html=True)
-            #st.components.v1.html(js_code, height=0)
-
-        # For human prompts
-        if line.startswith("Human: "):
-            contents = line.split("Human: ")[1]
-            st.markdown(get_chat_message(contents, align="right"), unsafe_allow_html=True)
-
-# Show feedback form if triggered by the bye intent
-if st.session_state.SHOW_FEEDBACK_FORM:
-    feedback()
+        
+    with st.sidebar:
+        if st.button("Clear Chat"):
+            clear_chat()
+            INITIAL_PROMPT = "Hello! How can I assist you today?"
+            st.session_state.LOG = [INITIAL_PROMPT]
+            st.session_state.LOG.append(f"AI: {INITIAL_PROMPT}")
+            st.session_state.MEMORY = [{'role': "system", 'content': INITIAL_PROMPT}]
     
-# Define an input box for human prompts
-with prompt_box:
-    human_prompt = st.chat_input("Ask me anything about FCSIT career", key=f"text_input_{len(st.session_state.LOG)}")
+    # Render chat history so far
+    with chat_box:
+        for line in st.session_state.LOG[1:]:
+            # For AI response
+            if line.startswith("AI: "):
+                contents = line.split("AI: ")[1]
+                st.markdown(get_chat_message(contents), unsafe_allow_html=True)
+                st.components.v1.html(js_code, height=0)
+                
+            # For human prompts
+            if line.startswith("Human: "):
+                contents = line.split("Human: ")[1]
+                st.markdown(get_chat_message(contents, align="right"), unsafe_allow_html=True)
 
-#Gate the subsequent chatbot response to only when the user has entered a prompt
-if human_prompt is not None and len(human_prompt) > 0:
-    run_res = asyncio.run(main(human_prompt))
-    if run_res['status'] == 0 and not DEBUG:
-        st.rerun()
+        #sidebar
+        with open("sidebar.md", "r") as sidebar_file:
+            sidebar_content = sidebar_file.read()
 
-    else:
-        if run_res['status'] != 0:
-            st.error(run_res['message'])
+        with open("style.css", "r") as styles_file:
+            styles_content = styles_file.read()
+
+        st.sidebar.markdown(sidebar_content)
+        st.write(styles_content, unsafe_allow_html=True)
+
+        # User feedback in the sidebar
+        st.sidebar.header("Feedback")
+        st.sidebar.markdown("We value your feedback. Click the feedback button to start your feedback!")
+        if(st.sidebar.button("User Feedback")):
+            st.session_state.SHOW_FEEDBACK_FORM = True
+
+        # Show feedback form if triggered by the bye intent
+        if st.session_state.SHOW_FEEDBACK_FORM:
+            feedback()
+            
+        # Define an input box for human prompts
         with prompt_box:
-            if st.button("Show text input field"):
+            human_prompt = st.chat_input("Ask me anything about FCSIT career", key=f"text_input_{len(st.session_state.LOG)}")
+
+        #Gate the subsequent chatbot response to only when the user has entered a prompt
+        if human_prompt is not None and len(human_prompt) > 0:
+            run_res = asyncio.run(main(human_prompt))
+            if run_res['status'] == 0 and not DEBUG:
                 st.rerun()
+
+            else:
+                if run_res['status'] != 0:
+                    st.error(run_res['message'])
+                with prompt_box:
+                    if st.button("Show text input field"):
+                        st.rerun()
